f27bb09c7dca55336a2ce19a423f4b0b583c32c2
adi <adpascubv@yahoo.ro>
2014-07-04 01:18:51 +0300

    fix marker orientation, ID and alter test cases

--- aruco-marker.js
+++ aruco-marker.js
@@ -44,9 +44,9 @@
         val = ids[index];
         for (x = 0; x < 5; x++) {
           if ((val >> (4 - x)) & 1) {
-            marker[x][y] = 1;
+            marker[x][4 - y] = 1;
           } else {
-            marker[x][y] = 0;
+            marker[x][4 - y] = 0;
           }
         }
       }
@@ -72,7 +72,7 @@
 
       for (y = 0; y < 5; y++) {
         for (x = 0; x < 5; x++) {
-          if (marker[x][y] === 1) {
+          if (marker[y][x] === 1) {
             image += '  <rect x="' + (x + 1) + '" y="' + (y + 1) +
               '" width="1" height="1" fill="white" ' +
               // Slight stroke to get around aliasing issues with adjacent rectangles
--- build/ng-aruco-marker.js
+++ build/ng-aruco-marker.js
@@ -1,4 +1,4 @@
-/*! aruco-marker 1.0.0 2014-06-19 - MIT Licensed, see http://github.com/bhollis/aruco-marker */
+/*! aruco-marker 1.0.0 2014-07-05 - MIT Licensed, see http://github.com/bhollis/aruco-marker */
 // AngularJS Directive for Aruco marker images
 // Usage:
 // <aruco-marker marker-id="{{markerId}}" size="500px"/>
