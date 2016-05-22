279645fea0e1523a0e023e1efd8255531ee041f0
Ben Hollis <ben@benhollis.net>
2016-05-21 20:56:19 -0700

    Update version, add AMD/CommonJS to angular

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
-0.1.0
+2.0.0
+-----
+
+* Fix marker orientation - it was diagonally inverted!
+* Add CommonJS and AMD support to the Angular directive.
+
+1.0.0
 -----
 
 * Initial release
--- ng-aruco-marker.js
+++ ng-aruco-marker.js
@@ -1,7 +1,23 @@
 // AngularJS Directive for Aruco marker images
 // Usage:
 // <aruco-marker marker-id="{{markerId}}" size="500px"/>
-(function() {
+(function (root, factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS
+    if (typeof angular === 'undefined') {
+      factory(require('angular'));
+    } else {
+      factory(angular);
+    }
+    module.exports = 'ngDialog';
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['angular'], factory);
+  } else {
+    // Global Variables
+    factory(root.angular);
+  }
+}(this, function (angular) {
   angular.module('arucoMarker', []).directive('arucoMarker', function() {
     return {
       restrict: 'E',
@@ -17,4 +33,4 @@
       }
     };
   });
-})();
+}));
